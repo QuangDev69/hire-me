6a10a516f5aae4c3e207900068a42a96f34ab5b9
QuangIDR <nguyenduyquangk0@gmail.com>
2024-06-13 10:46:43 +0700

    fix bug layout

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ const page = () => {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-14">
           <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Software Engineer</span>
             <h1 className="h1 mb-6">
@@ -16,7 +16,7 @@ const page = () => {
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80" />
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <div className="flex xl:flex-row gap-3 justify-center xl:justify-start items-center">
+              <div className="flex flex-col xl:flex-row gap-3 justify-center xl:justify-start items-center">
                 <a href="/assets/CV_NgDuyQuang.pdf" download>
                   <Button
                     variant="outline"
